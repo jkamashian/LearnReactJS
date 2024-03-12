a553425d73e4718a29471960d74bd9c564ea487b
jkamashian <jacobkamashian@gmail.com>
2024-03-12 15:58:08 -0600

    Example of state object

--- react-quiz/react-quiz/src/components/Quiz.js
+++ react-quiz/react-quiz/src/components/Quiz.js
@@ -3,17 +3,24 @@ import Question
  from "./Question";
 const Quiz = () => {
     console.log("render")
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [state, setState] = useState({
+        currentQuestionIndex: 0,
+        questions: []
+    });
     const testClick = () => {
         console.log('Test Click');
-        setCurrentQuestionIndex(currentQuestionIndex+1);
+        //setCurrentQuestionIndex(currentQuestionIndex+1);
+        setState({
+            ...state, 
+            currentQuestionIndex: state.currentQuestionIndex + 1,
+        });
     }
     return (
         <div className="quiz">
             <div>
                 <div className="score">Question 1/8</div>
                 <Question/>
-                <div className="next-button" onClick={testClick}>Next Question {currentQuestionIndex}</div>
+                <div className="next-button" onClick={testClick}>Next Question {state.currentQuestionIndex}</div>
             </div>
         </div>
     );
