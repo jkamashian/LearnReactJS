d1259d8d453080e73fa082e34be35fe2f6f10526
jkamashian <jacobkamashian@gmail.com>
2024-03-12 15:51:06 -0600

    Now using a stateful question

--- react-quiz/react-quiz/src/components/Quiz.js
+++ react-quiz/react-quiz/src/components/Quiz.js
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import Question
  from "./Question";
 const Quiz = () => {
+    console.log("render")
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const testClick = () => {
+        console.log('Test Click');
+        setCurrentQuestionIndex(currentQuestionIndex+1);
+    }
     return (
         <div className="quiz">
             <div>
                 <div className="score">Question 1/8</div>
                 <Question/>
-                <div className="next-button">Next Question</div>
+                <div className="next-button" onClick={testClick}>Next Question {currentQuestionIndex}</div>
             </div>
         </div>
     );
-};
+}
 
 export default Quiz;
